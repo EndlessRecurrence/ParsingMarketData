e8f24863d2ffe7515ca69715612125627989b7b9
EndlessRecurrence <georgesut@yahoo.com>
2023-03-16 15:00:19 +0200

    Initialize project with Mix

---
+++ market_data/lib/market_data.ex
@@ -0,0 +1,18 @@
+defmodule MarketData do
+  @moduledoc """
+  Documentation for `MarketData`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MarketData.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ market_data/test/market_data_test.exs
@@ -0,0 +1,8 @@
+defmodule MarketDataTest do
+  use ExUnit.Case
+  doctest MarketData
+
+  test "greets the world" do
+    assert MarketData.hello() == :world
+  end
+end
