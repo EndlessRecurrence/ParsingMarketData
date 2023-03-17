a9550234be0c92ff1d5b30f062c5c750c1530345
EndlessRecurrence <georgesut@yahoo.com>
2023-03-17 12:08:11 +0200

    Read file and add values as strings into a map

--- market_data/lib/market_data.ex
+++ market_data/lib/market_data.ex
@@ -1,18 +1,54 @@
 defmodule MarketData do
   @moduledoc """
-  Documentation for `MarketData`.
+    Functions for processing a large file containing stock market data.
   """
 
-  @doc """
-  Hello world.
+  def main do
+    read_data_as_text_from_file("res/data.txt")
+  end
 
-  ## Examples
+  def read_data_as_text_from_file(path) do
+    read_lines_from_file(path)
+      |> create_map_of_properties()
+  end
 
-      iex> MarketData.hello()
-      :world
+  def read_lines_from_file(path) do
+    lines =
+      path
+      |> File.stream!()
+      |> Enum.map(&String.trim/1)
 
-  """
-  def hello do
-    :world
+    lines |> Enum.map(&split_line_into_tokens(&1))
+  end
+
+  def split_line_into_tokens(line) do
+    Regex.split(~r/\s+/, line)
+  end
+
+  def create_map_of_properties(split_lines) do
+    properties = split_lines
+      |> Enum.at(0)
+      |> Enum.map(&String.to_atom(&1))
+
+    [_ | rest_of_the_lines] = split_lines
+    map = Enum.reduce(properties, %{}, fn x, acc ->
+      Map.put(acc, x, [])
+    end)
+
+    add_values_to_map(rest_of_the_lines, properties, map)
+  end
+
+  def add_values_to_map([], _, map), do: map
+  def add_values_to_map([line | lines], properties, map) do
+    row_as_map =
+      Enum.zip(0..length(line), line)
+      |> Enum.map(fn {index, value} -> {Enum.at(properties, index), value} end)
+      |> Keyword.new()
+      |> Enum.into(%{})
+
+    map = Map.merge(map, row_as_map, fn key, v1, v2 ->
+      [v2 | v1]
+    end)
+    add_values_to_map(lines, properties, map)
   end
 end
